88d56a53aa768e230b100ef41a4f94ec8d533d78
Shawn Otomo <kodama78@gmail.com>
2019-04-25 16:37:04 -0700

    first commit

--- server/server.js
+++ server/server.js
@@ -12,15 +12,21 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://shawn:shawn@lyricaldb-dfem0.mongodb.net/test?retryWrites=true';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
 // Connect to the mongoDB instance and log a message
 // on success or failure
-mongoose.connect(MONGO_URI);
-mongoose.connection
+mongoose.connect(MONGO_URI, {
+	authSource: 'admin',
+	retryWrites: true,
+	dbName: 'auth',
+	useCreateIndex: true,useNewUrlParser: true
+});
+
+const db = mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
 
@@ -34,7 +40,8 @@ app.use(session({
   saveUninitialized: true,
   secret: 'aaabbbccc',
   store: new MongoStore({
-    url: MONGO_URI,
+    // url: MONGO_URI,
+    mongooseConnection: db,
     autoReconnect: true
   })
 }));
